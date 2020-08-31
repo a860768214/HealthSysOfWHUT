e02dc2de333883749d8b79319161426f321e4300
a860768214 <860768214@qq.com>
2020-08-31 21:28:48 +0800

    "fourth commit"

--- src/main/java/com/whut/health_sys/config/CodeConfig.java
+++ src/main/java/com/whut/health_sys/config/CodeConfig.java
@@ -2,23 +2,23 @@ package com.whut.health_sys.config;
 
 public class CodeConfig
 {
-    public static int STATUS_USER_STUDENT=1;
-    public static int STATUS_USER_DOC=2;
-    public static int STATUS_USER_EXPIRED=0;
-    public static int STATUS_USER_ADMIN=-1;
+    public static final int STATUS_USER_STUDENT=1;
+    public static final int STATUS_USER_DOC=2;
+    public static final int STATUS_USER_EXPIRED=0;
+    public static final int STATUS_USER_ADMIN=-1;
 
-    public static int STATUS_DOC_NORMAL=1;
-    public static int STATUS_DOC_EXPIRED=0;
+    public static final int STATUS_DOC_NORMAL=1;
+    public static final int STATUS_DOC_EXPIRED=0;
 
-    public static int STATUS_CLINIC_NORMAL=1;
-    public static int STATUS_CLINIC_EXPIRED=0;
+    public static final int STATUS_CLINIC_NORMAL=1;
+    public static final int STATUS_CLINIC_EXPIRED=0;
 
-    public static int STATUS_RES_ACCESSED=1;
-    public static int STATUS_RES_DENIED=0;
-    public static int STATUS_RES_PROCESSED=2;
-    public static int STATUS_RES_FINISHED=-1;
+    public static final int STATUS_RES_ACCESSED=1;
+    public static final int STATUS_RES_DENIED=0;
+    public static final int STATUS_RES_PROCESSED=2;
+    public static final int STATUS_RES_FINISHED=-1;
 
-    public static int STATUS_APPOINT_ACCESSED=1;
-    public static int STATUS_APPOINT_DENIED=0;
-    public static int STATUS_APPOINT_PROCESSED=-1;
+    public static final int STATUS_APPOINT_ACCESSED=1;
+    public static final int STATUS_APPOINT_DENIED=0;
+    public static final int STATUS_APPOINT_PROCESSED=-1;
 }
